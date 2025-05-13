a902b152bab442a65a5519e62eb3aa85267997f6
Luke <lukefwilson@gmail.com>
2025-05-13 15:12:41 +0100

    shuffle stakes when picking validator

--- BlockchainAssignment/ValidatorPicker.cs
+++ BlockchainAssignment/ValidatorPicker.cs
@@ -16,18 +16,21 @@ namespace BlockchainAssignment
             if (totalStake <= 0)
                 throw new InvalidOperationException("Total stake must be positive to select a validator.");
 
+            // Shuffle the stakes to remove any order bias
+            var shuffledStakes = stakes.OrderBy(_ => random.Next()).ToList();
+
             // Generate a random value in [0, totalStake)
-            double sample = random.NextDouble() * (double)totalStake;
+            double sample = random.NextDouble() * totalStake;
             double cumulative = 0;
 
-            foreach (var kvp in stakes)
+            foreach (var kvp in shuffledStakes)
             {
                 cumulative += kvp.Value;
-                if ((double)cumulative >= sample)
+                if (cumulative >= sample)
                     return kvp.Key;
             }
 
-            return stakes.Keys.Last();
+            return shuffledStakes.Last().Key;
         }
     }
 }
